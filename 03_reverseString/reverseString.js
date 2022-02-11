d35f3f5f6694eae6868395067cb8fbd44ce26fb1
deliags <deliagsmith@outlook.com>
2022-02-11 10:27:50 +0000

    Implemented Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
 
+  //String is split into array
+  let splitString = string.split('');
+  //Reverse the array
+  let reverseString = splitString.reverse();
+  //Convert the array into a string
+  let joinString = reverseString.join('');
+  
+  return joinString;
 };
 
 // Do not edit below this line
