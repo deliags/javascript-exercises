ec5b2eb625144c939115fd98fc2f7a74897f4a5e
deliags <deliagsmith@outlook.com>
2022-02-13 18:50:03 +0000

    Working solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(farDegrees) {
+  let celDegrees = (farDegrees - 32) * 5 / 9;
+  let roundedCelDegrees = Math.round(celDegrees * 10) / 10;
+  return roundedCelDegrees;
 
 };
 
-const ctof = function() {
-
+const ctof = function(celDegrees) {
+  let farDegrees = (celDegrees * 9 / 5) + 32;
+  let roundedFarDegrees = Math.round(farDegrees * 10) / 10;
+  return roundedFarDegrees;
 };
 
 // Do not edit below this line
