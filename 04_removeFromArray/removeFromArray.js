3bdc0b52eb58494a2be471491964ae1bd41bfd06
deliags <deliagsmith@outlook.com>
2022-02-13 12:47:53 +0000

    Working solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  let tempArray = array;
+  let args = values;
+  let filteredArray = tempArray.filter(element => !args.includes(element));
 
+  return filteredArray;
 };
 
 // Do not edit below this line
