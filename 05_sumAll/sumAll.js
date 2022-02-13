2f86a122c62e28fef590409c74ac738a858c78de
deliags <deliagsmith@outlook.com>
2022-02-13 15:36:32 +0000

    Working solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt) {
 
+  let total = 0;
+
+  while (startInt > 0 && typeof (startInt) && typeof (endInt) == 'number') {
+    
+    //Starts at zero and adds the start number, then decreases it until it is equal to the end number
+    if (startInt > endInt) {
+      for (let i = startInt; i >= endInt; i--) { total += i; }
+    }
+    else {
+      for (let i = 1; i <= endInt; i++) { total += i; }
+    }
+    return total;
+  }
+  //Covers invalid entries
+  return 'ERROR';
 };
 
 // Do not edit below this line
