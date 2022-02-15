f56ac57cfc9c6442f429b6d13542fc7ab072d444
deliags <deliagsmith@outlook.com>
2022-02-15 09:39:30 +0000

    Working solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (firstNum, secNum) {
+  let addSum = firstNum + secNum;
+  return addSum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNum, secNum) {
+  let subtraction = firstNum - secNum;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  // 0 + 1 + 2 + 3 + 4
+  const initialValue = 0;
+  const sumWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, initialValue)
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  
+  const sumWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue)
+  return sumWithInitial;
 };
 
-const power = function() {
-	
+const power = function (firstNum, secNum) {
+  return firstNum ** secNum;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0){
+    return 1;
+  }
+ 
+  let product = 1; 
+  
+  for (let i = number; i > 0; i--){
+    product *= i;
+  }
+  return product; 
 };
 
 // Do not edit below this line
