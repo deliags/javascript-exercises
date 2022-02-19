05c1c5a9880b25e0eea0036f209d0734d53837ee
deliagsmith <66247902+deliags@users.noreply.github.com>
2022-02-19 14:23:41 +0000

    removed unnecessary comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -7,18 +7,6 @@ const palindromes = function (str) {
   
     /* Check to see if str is a Palindrome*/
      return (removeChar === checkPalindrome);
-  // // find the length of a string
-  // const len = string.length;
-
-  // // loop through half of the string
-  // for (let i = 0; i < len / 2; i++) {
-
-  //     // check if first and last string are same
-  //     if (string[i] !== string[len - 1 - i]) {
-  //         return false;
-  //     }
-  // }
-  // return true;
 };
 
 // Do not edit below this line
